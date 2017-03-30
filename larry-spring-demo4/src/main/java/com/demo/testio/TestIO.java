c9dd42da909c5a343f18ebf9aed85c1f71533f6b
Fwd@T460 <fanweidong2014@163.com>
2017-03-30 13:40:31 +0800

    提交TestIO.java

--- larry-spring-demo4/src/main/java/com/demo/testio/TestIO.java
+++ larry-spring-demo4/src/main/java/com/demo/testio/TestIO.java
@@ -1,10 +1,16 @@
 package com.demo.testio;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -18,30 +24,158 @@ public class TestIO {
 	private static Logger LOG = LoggerFactory.getLogger(TestIO.class);
 
 	public static void main(String[] args){
-//		TestIO t = new TestIO();
-//		URL url = t.getClass().getResource("");
-//		LOG.info(""+url.toString());
-//		String str = t.getClass().getResource("/")+"";
-//		LOG.info("str:"+str);
-		try {
-			readFile();
-		} catch (URISyntaxException | IOException e) {
+//		createFile();
+//		readFile();
+//		copyFile();
+//		objectStream();
+		dataStream();
+	}
+	
+	public static void dataStream(){
+		URI uri;
+		File f;
+		DataInputStream dataInput = null;
+		DataOutputStream dataOutput = null;
+		try{
+			uri = new URI("file:/E:/data.txt");
+			f = new File(uri);
+			dataOutput = new DataOutputStream(new FileOutputStream(f));
+			Pojo p = new Pojo();
+			p.setCount(1);
+			p.setName("1");
+			dataOutput.writeUTF(p.getName());
+			dataOutput.writeInt(p.getCount());
+			dataOutput.flush();
+			dataInput = new DataInputStream(new FileInputStream(f));
+			String name = dataInput.readUTF();
+			int count = dataInput.readInt();
+			System.out.println("name:"+name);
+			System.out.println("count:"+count);
+		} catch (URISyntaxException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			try{
+				dataInput.close();
+				dataOutput.close();
+			}catch(IOException e){
+				e.printStackTrace();
+			}
 		}
 	}
 	
-	public static void readFile() throws URISyntaxException, IOException{
-		URI uri = new URI("file:/E:/readFile.txt");
-		File f = new File(uri);
-		InputStream inputStream = null;
-		inputStream = new FileInputStream(f);
-		int count = 0;
-		while(inputStream.read() != -1){
-			count++;
+	/**
+	 * object对象
+	 */
+	public static void objectStream(){
+		URI uri = null;
+		File f = null;
+		ObjectInputStream objectInput = null;
+		ObjectOutputStream objectOutput = null;
+		try {
+			uri = new URI("file:/E:/student.txt");
+			f = new File(uri);
+			objectOutput = new ObjectOutputStream(new FileOutputStream(f));
+			objectOutput.writeObject(new Student("fwd", 27));
+			objectOutput.writeObject(new Student("gxm", 27));
+			
+			objectInput = new ObjectInputStream(new FileInputStream(f));
+//			System.out.println(objectInput.readObject());
+//			System.out.println(objectInput.readObject());
+			Student s = (Student)objectInput.readObject();
+			System.out.println(s.toString());
+			LOG.info("end");
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			// TODO: handle finally clause
+			try {
+				objectOutput.close();
+				objectInput.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		LOG.info("read length:"+count);
-		inputStream.close();
+	}
+	
+	/**
+	 * 复制文件
+	 */
+	public static void copyFile(){
+		URI uri = null;
+		File fIn = null;
+		File fOut = null;
+		FileInputStream fiStream = null;
+		FileOutputStream foStream = null;
+		byte[] b = new byte[512];
+		try{
+			uri = new URI("file:/E:/nclcore.jar");
+			fIn = new File(uri);
+			uri = new URI("file:/E:/nclcorebak.jar");
+			fOut = new File(uri);
+			fiStream = new FileInputStream(fIn);
+			foStream = new FileOutputStream(fOut);
+			
+			while(fiStream.read(b)!=-1){
+				foStream.write(b);
+			}
+			
+			LOG.info("end");
+			
+		}catch(URISyntaxException ex){
+			LOG.error("copyFile Exception", ex);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			try {
+				fiStream.close();
+				foStream.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * 读文件
+	 */
+	public static void readFile(){
+		try{
+			URI uri = new URI("file:/E:/readFile.txt");
+			File f = new File(uri);
+			InputStream inputStream = null;
+			inputStream = new FileInputStream(f);
+			int count = 0;
+			while(inputStream.read() != -1){
+				count++;
+			}
+			LOG.info("read length:"+count);
+			inputStream.close();
+		}catch(Exception ex){
+			LOG.error("readFileException", ex);
+		}
 	}
 	
 	/**
@@ -72,5 +206,46 @@ public class TestIO {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+}
+
+class Student implements Serializable{
+
+	/**
+	 * autu generated
+	 */
+	private static final long serialVersionUID = 2275373136784309264L;
+	
+	private String name;
+	private int age;
+	
+	Student(String name, int age){
+		this.name = name;
+		this.age = age;
+	}
+	
+	@Override
+	public String toString(){
+		String str = "Student {name:"+name+", age:"+age+"}";
+		return str;
+	}
+	
+}
+
+class Pojo{
+	private String name;
+	private int count;
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return this.name;
+	}
+	public void setCount(int count){
+		this.count = count;
+	}
+	public int getCount(){
+		return count;
 	}
 }
